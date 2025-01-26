9f3484019680985620ff5cd04e4ec9b544e72489
gzimeminii <gzimeminii@gmail.com>
2025-01-26 15:22:05 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
-          port: "",
-          pathname: "/**",
-        },
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
-  
-  
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
